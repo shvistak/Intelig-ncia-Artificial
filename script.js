659fdfb1a1ecf7f8407fc592243334c95b3be795
Amanda <108832388+shvistak@users.noreply.github.com>
2024-06-18 11:34:30 -0300

    1806

---
+++ script.js
@@ -0,0 +1,26 @@
+const caixaPrincipal = document.querySelector('.caixa-principal');
+const caixaPerguntas = document.querySelector('.caixa-perguntas');
+const caixaAlternativas = document.querySelector('.caixa-alternativas');
+const caixaResultados = document.querySelector('.caixa-resultados');
+const textoResultado = document.querySelector('.texto-resultado');
+const lista = [item1, item2]
+const perguntas = [
+    {
+        enunciado: "Pergunta 1",
+        alternativas: ["Alternativa 1", "Alternativa 2"],
+    },
+    {
+        enunciado: "Pergunta 2",
+        alternativas: ["Alternativa 1", "Alternativa 2"],
+    },
+    {
+        enunciado: "Pergunta 3",
+        alternativas: ["Alternativa 1", "Alternativa 2"],
+    },
+];
+const lapis = {
+    tamanho: 20,
+    tipo:'HB' ,
+    cor: 'Grafite',
+    temBorrachaAtras: false
+}
