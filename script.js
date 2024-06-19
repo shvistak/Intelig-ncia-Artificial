57d5d3cbf1ad978c17c9db970680ad2390446920
Amanda <108832388+shvistak@users.noreply.github.com>
2024-06-19 15:00:15 -0300

    1906

--- script.js
+++ script.js
@@ -18,6 +18,21 @@ const perguntas = [
         alternativas: ["Alternativa 1", "Alternativa 2"],
     },
 ];
+let atual = 0;
+let perguntaAtual;
+function mostraPergunta(){
+    perguntaAtual = perguntas [atual];
+    caixaPerguntas.textContent = perguntaAtual.enunciado;
+    mostraAlternativas ();
+}
+function mostraAlternativas(){
+    for (const alternativa of perguntaAtual.alternativas){
+        const botaoAlternativas = document.createElement("button");
+        botaoAlternativas.textContent = alternativa;
+        caixaAlternativas.appendChild(botaoAlternativas);
+    }
+}
+mostraPergunta();
 const lapis = {
     tamanho: 20,
     tipo:'HB' ,
