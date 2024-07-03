595d65fe3a76b7deefad6d3ad95d3ee44dfe43bc
Amanda <108832388+shvistak@users.noreply.github.com>
2024-07-03 11:44:22 -0300

    0306

--- script.js
+++ script.js
@@ -7,9 +7,19 @@ const lista = [item1, item2]
 const perguntas = [
     {
         enunciado: "Assim que saiu da escola Gabriel se depara com uma nova tecnologia, um chat que consegue responder todas as dúvidas que uma pessoa pode ter, o chat também gera imagens e áudios hiper-realistas. Qual o primeiro pensamento de Gabriel?",
-        alternativas: ["Isso é assustador!", "Isso é maravilhoso!"],
-    },
+        alternativas: [
+            "Isso é assustador!", 
+            "Isso é maravilhoso!"],
+        },
     {
+        texto: "Isso é assustador!",
+            afirmação: "Afirmação 1"
+        },
+        {
+            texto: "Isso é maravilhoso!",
+            afirmação: "Afirmação 2"
+        }
+        ],
         enunciado: "Com a descoberta desta tecnologia uma professora de tecnologia da escola decidiu fazer uma sequência de aulas sobre IA. No fim de uma aula ela pede que Gabriel escreva um trabalho sobre o uso de tecnologia em sala de aula. Qual atitude Gabriel toma?",
         alternativas: ["Utiliza uma ferramenta de busca na internet que utiliza IA para que ela ajude a encontrar informações relevantes para o trabalho e explique numa linguagem que facilite o entendimento", "Escreve o trabalho com base nas conversas que teve com colegas, algumas pesquisas na internet e conhecimentos próprios sobre o tema."],
     },
